27f206e52f99c1728e3be4e6ce628ff495f7f3eb
hieudeptrai4603 <hieudeptrai4603@gmail.com>
2025-02-11 22:31:24 +0700

    Update thumbnail image in ProfileView

--- InstagramClone/Views/CollectionView Headers/ProfileInfoHeaderCollectionReusableView.swift
+++ InstagramClone/Views/CollectionView Headers/ProfileInfoHeaderCollectionReusableView.swift
@@ -8,5 +8,14 @@
 import UIKit
 
 class ProfileInfoHeaderCollectionReusableView: UICollectionReusableView {
-        static let identifier = "ProfileInfoHeaderCollectionReusableView"
+    static let identifier = "ProfileInfoHeaderCollectionReusableView"
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .systemBlue
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
--- Podfile
+++ Podfile
@@ -1,12 +1,13 @@
 project 'InstagramClone.xcodeproj'
 
-platform :ios, '13.0'
+platform :ios, '14.0'
 
 target 'InstagramClone' do
   # Comment the next line if you don't want to use dynamic frameworks
   use_frameworks!
 
   # Pods for InstagramClone
+  pod 'SDWebImage'
   pod "Appirater"
   pod 'FirebaseCore'
   pod 'FirebaseAuth'
